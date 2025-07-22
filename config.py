e75202e48927346d37779d279b85491c75f887e9
Casper112 <scalesprojectpro@gmail.com>
2025-07-23 01:13:18 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,12 @@
+api_id = 28479008
+api_hash = 'ac7f39c6b95b4d67865abc396ac07523'
+bot_token = '7948745764:AAHUTyspRcKqi4GdCaAlhX2-0ehNQWQY9m0'
+my_id = 8040507334
+
+technical_channel_id = -1002637752457   # ID технического канала
+
+new_link = "http://t.me/"
+new_username = "@"
+
+proxy_url = 'http://ip:port'  #заполнять только если планируете рерайтить текст в режиме модерации с помощью Chat GPT
+openai_api_key = "sk-..."  #заполнять только если планируете рерайтить текст в режиме модерации с помощью Chat GPT
